7d944c911a5327ae97f8da61d8bb87f5a0900ffb
antoniocuga <antoniocuga@gmail.com>
2014-02-01 11:00:27 -0500

    Añadiendo campo de cantidad

--- sabado04/productos/js/script.js
+++ sabado04/productos/js/script.js
@@ -1,9 +1,9 @@
 var products = [
-    {"id": "tv42", "product": "Televisor 42'", 'category': "Electro"},
-    {"id": "tv60", "product": "Televisor 60'", 'category': "Electro"},
-    {"id": "tv32", "product": "Televisor 32'", 'category': "Electro"},
-    {"id": "eqson", "product": "Equipo de sonido", 'category': "Sonido"},
-    {"id": "dvd", "product": "DVD Panasonic'", 'category': "Relax"}
+    {"id": "tv42", "product": "Televisor 42'", "cantidad": 10, 'category': "Electro"},
+    {"id": "tv60", "product": "Televisor 60'", "cantidad": 10, 'category': "Electro"},
+    {"id": "tv32", "product": "Televisor 32'", "cantidad": 10, 'category': "Electro"},
+    {"id": "eqson", "product": "Equipo de sonido", "cantidad": 10, 'category': "Sonido"},
+    {"id": "dvd", "product": "DVD Panasonic'", "cantidad": 10, 'category': "Relax"}
 ];
 
 $(document).ready(function() {
@@ -19,6 +19,11 @@ $(document).ready(function() {
 
 
     });
+    $('.producto > a').click(function() {
+
+        var id = $(this).attr('id-product');
+        renderProducts(id, 'compras');
+    });
 
 });
 
@@ -27,26 +32,38 @@ function loadproducts() {
 
     for (var i in products) {
 
-        var div = document.createElement('div');
-        var producto = document.createElement('span');
-        var category = document.createElement('span');
-        var id = document.createElement('a');
-
-        $(producto).append(products[i].product);
-        $(producto).addClass('producto-name');
-        $(category).append(products[i].category);
-        $(category).addClass('producto-categoria');
-        $(category).addClass(products[i].category);
-        $(id).attr({'href': '#', 'id-product': products[i].id});
-        $(id).append("Comprar");
-        $(id).addClass('producto-id');
-
-        div.appendChild(producto);
-        div.appendChild(category);
-        div.appendChild(id);
-        div.setAttribute('class', 'producto ' + products[i].category);
-        div.setAttribute('index', i);
-        $('#productos').append(div);
+        renderProducts(i, 'productos');
     }
+
+}
+
+//@i is the product position
+//@container is the ID for the div content, only ID madafaka
+function renderProducts(i, container) {
+
+    var div = document.createElement('div');
+    var producto = document.createElement('span');
+    var category = document.createElement('span');
+    var id = document.createElement('a');
+    var cantidad = document.createElement('span');
+
+    $(producto).append(products[i].product);
+    $(producto).addClass('producto-name');
+    $(category).append(products[i].category);
+    $(category).addClass('producto-categoria');
+    $(category).addClass(products[i].category);
+    $(id).attr({'href': '#', 'id-product': i});
+    $(id).append("Comprar");
+    $(id).addClass('producto-id');
+    $(cantidad).append(products[i].cantidad);
+    $(cantidad).addClass('producto-cantidad');
+
+    div.appendChild(producto);
+    div.appendChild(category);
+    div.appendChild(id);
+    div.appendChild(cantidad);
+    div.setAttribute('class', 'producto ' + products[i].category);
+    div.setAttribute('index', i);
+    $('#' + container).append(div);
 
 }
